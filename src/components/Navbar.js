c47cdafd35506ec3ac5fbb265ed4770015f46ca7
Daniel Karanja <sirkaranja12@gmail.com>
2023-04-24 14:46:50 +0300

    navigation bar file

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar () {
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div style={{justifyContent : "center"}} className="container-fluid">
+                <span className="navbar-text">
+                    <h1>Bank of Flatiron</h1>
+                </span>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
