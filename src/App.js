a08ce9f989632d88c6339e54ab969bbe9ae46157
Daniel Karanja <sirkaranja12@gmail.com>
2023-04-24 14:32:37 +0300

    fetching transaction eith useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useSate, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Table from './components/Table';
+import Form from './components/Form';
 
 function App() {
+  const [transactions, setTransactions] = useState([])
+
+  //fetch data from the db.json using useEffect
+
+
+ 
+  useEffect (()=> {
+    fetch ("http://localhost:3000/transactions")
+      .then ((r)=> r.json ())
+      .then ((data)=> setTransactions(data))
+  }, [])
+
+  function updatedTransactions (newData) {
+    const updatedTransactionsArray = [...transactions, newData]
+    setTransactions (updatedTransactionsArray)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/><br/>
+      <Form newTransaction = {updatedTransactions}/><br/>
+      <Table arayOfAllTransactions = {transactions}/>
     </div>
   );
 }
